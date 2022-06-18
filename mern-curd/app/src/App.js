233676c980b0f57b775f6618065a60ea5eebe32b
premprakash8080 <84430639+premprakash8080@users.noreply.github.com>
2022-06-18 19:10:39 +0530

    Add files via upload

---
+++ mern-curd/app/src/App.js
@@ -0,0 +1,26 @@
+import AllUsers from './Component/AllUsers';
+import AddUser from './Component/AddUser';
+import EditUser from './Component/EditUser';
+import NavBar from './Component/NavBar';
+import NotFound from './Component/NotFound'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBarFooter from './Component/Footer';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+
+        <Route path="/" element={<AllUsers /> } />
+        <Route path="/all" element={<AllUsers /> } />
+        <Route path="/add" element={<AddUser />} />
+        <Route path="/edit/:id" element={<EditUser />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+      <NavBarFooter/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ mern-curd/app/src/Component/Footer.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+export default function Footer() {
+  return (
+    <div    style={{
+        position: "fixed",
+        left: 0,
+        bottom: 0,
+        right: 0,
+        color:'white',
+        textAlign:'center',
+        backgroundColor: "black"
+      }} >
+          <h2>CURD ||@</h2>
+        </div>
+  );
+}
